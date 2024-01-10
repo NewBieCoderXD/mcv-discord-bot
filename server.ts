0d97f7602c3e350b74d8040f76039d6328fb66a9
NewBieCOderXD <fruity5555@gmail.com>
2024-01-10 09:46:02 +0700

    get assignments

--- server.ts
+++ server.ts
@@ -5,11 +5,17 @@ dotenv.config({
 })
 
 !async function start(){
+    if(process.env["COOKIE"]==undefined){
+        return;
+    }
     let result = await fetch("https://www.mycourseville.com/?q=courseville/course/37700/assignment",{
+        method: "get",
         headers:{
             Cookie:process.env["COOKIE"]
         }
     }).then((res)=>res.text())
     const $ = cheerio.load(result);
-    console.log($("#cv-assignment-table tr"))
+    console.log($(("#cv-assignment-table tbody tr td:nth-child(2) a")).map((i:number,ele:cheerio.Element)=>{
+        return $(ele).html();
+    }).get());
 }()
